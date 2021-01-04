6ced87a70a65a879a7657126383bace7597eb79e
omerson-cruz <omerson.e.cruz@gmail.com>
2021-01-04 17:14:38 +0800

    Changed config

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-git push -f git@github.com:omerson-cruz/vue-stock-trader.git master:gh-pages
+git push -f git@github.com:omerson-cruz/math-quiz-app.git master:gh-pages
 
 cd -
